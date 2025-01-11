2e98aa47bffca2fbd570b08858c12002514cf8f2
matias ramos <matiasramos261@gmail.com>
2025-01-11 11:04:45 -0300

    cors

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -3,7 +3,7 @@ import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
-@Controller('product')
+@Controller('api/products')
 export class ProductController {
   constructor(private readonly productService: ProductService) { }
 
